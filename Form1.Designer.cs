d5b4421dfaa1764519730c13ab43032a0267f002
YeonBi01 <vonhathoangg@gmail.com>
2024-05-29 15:56:15 +0700

    doan

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DoAn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel_Outer = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Panel_Outer
+            // 
+            this.Panel_Outer.AutoScroll = true;
+            this.Panel_Outer.Location = new System.Drawing.Point(5, 2);
+            this.Panel_Outer.Name = "Panel_Outer";
+            this.Panel_Outer.Size = new System.Drawing.Size(1330, 832);
+            this.Panel_Outer.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1336, 837);
+            this.Controls.Add(this.Panel_Outer);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1352, 876);
+            this.MinimumSize = new System.Drawing.Size(1352, 876);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Panel_Outer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace DoAn
+{
+    public partial class Form1 : Form
+    {
+        private HtmlAgilityPack.HtmlDocument document;
+        List<Film> films;
+        private WebClient myClient;
+
+        public class Film
+        {
+            public string Link_Img { get; set; }
+            public string Link_Uri { get; set; }
+            public string Film_Name { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load_Movies();
+        }
+
+       private void Load_Movies()
+        {
+            myClient = new WebClient();
+            myClient.Encoding = System.Text.Encoding.UTF8;
+            Uri uri = new Uri("https://betacinemas.vn/phim.htm");
+            Stream replied = myClient.OpenRead(uri);
+
+
+            string html = myClient.DownloadString(uri);
+            document = new HtmlAgilityPack.HtmlDocument();
+            document.LoadHtml(html);
+
+
+            replied.Close();
+            myClient.Dispose();
+
+
+            films = new List<Film>();
+            if (document == null) return;
+            var film_List = document.DocumentNode.SelectNodes("//*[@id=\"tab-1\"]/div/div/div/div/div/div/img");
+            film_List.ToList().ForEach(film =>
+            {
+                films.Add(new Film { Link_Img = film.Attributes["src"].Value });
+            });
+
+            film_List = document.DocumentNode.SelectNodes("//*[@id=\"tab-1\"]/div/div/div/div[2]/div/h3/a");
+            var temp_list = film_List.ToList();
+            for (int i = 0; i < temp_list.Count; i++)
+            {
+                films[i].Link_Uri = "https://betacinemas.vn" + temp_list[i].Attributes["href"].Value;
+                films[i].Film_Name = temp_list[i].InnerText;
+            }
+
+            List<Panel> panels = new List<Panel>();
+
+            for (int i = 0; i < films.Count; i++)
+            {
+                // 
+                // Pbx_MoviePoster
+                //
+                PictureBox Pbx_MoviePoster = new PictureBox();
+                Pbx_MoviePoster.Location = new System.Drawing.Point(0, 0);
+                Pbx_MoviePoster.Name = "Pbx_MoviePoster";
+                Pbx_MoviePoster.Size = new System.Drawing.Size(315, 406);
+                Pbx_MoviePoster.TabIndex = 0;
+                Pbx_MoviePoster.TabStop = false;
+                Pbx_MoviePoster.ImageLocation = films[i].Link_Img;
+                // 
+                // Label_MovieName
+                //
+                Label Label_MovieName= new Label();
+                
+                Label_MovieName.AutoSize = true;
+                Label_MovieName.Location = new System.Drawing.Point(0, 409);
+                Label_MovieName.MaximumSize = new System.Drawing.Size(314, 75);
+                Label_MovieName.Name = "Label_MovieName";
+                Label_MovieName.Size = new System.Drawing.Size(286, 42);
+                Label_MovieName.TabIndex = 1;
+                Label_MovieName.Text = films[i].Film_Name;
+
+                Panel Panel_Carousel = new Panel();
+                Panel_Carousel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                Panel_Carousel.Controls.Add(Label_MovieName);
+                Panel_Carousel.Controls.Add(Pbx_MoviePoster);
+                Panel_Carousel.Location = new System.Drawing.Point(56 + 315 *(i%3), 10+478 *(i/3));
+                Panel_Carousel.Name = "Panel_Carousel";
+                Panel_Carousel.Size = new System.Drawing.Size(315, 478);
+                Panel_Carousel.TabIndex = 0;
+
+                this.Panel_Outer.Controls.Add(Panel_Carousel);
+            }
+        }
+    }
+}
